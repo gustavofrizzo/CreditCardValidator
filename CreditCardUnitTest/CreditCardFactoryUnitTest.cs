dfda575bf96a55168fb796ac013645020e8e145a
gustavofrizzo <guto.smo@gmail.com>
2015-07-02 03:47:49 -0300

    Creating UnitTests.

---
+++ CreditCardUnitTest/CreditCardFactoryUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CreditCardValidator;
+
+namespace CreditCardUnitTest
+{
+    [TestClass]
+    public class CreditCardFactoryUnitTest
+    {
+        [TestMethod]
+        public void CreatingRandomCardNumber()
+        {
+            String number = CreditCardFactory.RandomCardNumber(CardIssuer.MasterCard);
+
+            CreditCardDetector card = new CreditCardDetector(number);
+
+            Assert.IsTrue(card.Brand == CardIssuer.MasterCard);
+            Assert.IsTrue(card.IsValid(CardIssuer.MasterCard));
+        }
+    }
+}
