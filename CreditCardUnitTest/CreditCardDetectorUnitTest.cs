5cb63e08733be28c6ba1b45d4ddb36fdbff47011
gustavofrizzo <guto.smo@gmail.com>
2015-07-03 02:37:46 -0300

    Increasing the tests coverage.

---
+++ CreditCardUnitTest/CreditCardDetectorUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using CreditCardValidator;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CreditCardUnitTest
+{
+    [TestClass]
+    public class CreditCardDetectorUnitTest
+    {
+        [TestMethod]
+        public void Validating_CreditCardDetector()
+        {
+            using (StreamReader r = new StreamReader(@"Data\ValidCards.json"))
+            {
+                string json = r.ReadToEnd();
+                var allCardNumbers = JsonConvert.DeserializeObject<Dictionary<String, String[]>>(json);
+
+                foreach (var cardsBrand in allCardNumbers)
+                {
+                    foreach (var number in cardsBrand.Value)
+                    {
+                        CreditCardDetector detector = new CreditCardDetector(number);
+
+                        Assert.IsTrue(detector.IsValid());
+                        Assert.AreEqual(cardsBrand.Key, detector.Brand.ToString());
+                    }
+                }
+
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InvalidNumbers_CreditCardDetector()
+        {
+            String invalidNumber = "";
+
+            CreditCardDetector detc = new CreditCardDetector(invalidNumber);
+        }
+
+    }
+}
