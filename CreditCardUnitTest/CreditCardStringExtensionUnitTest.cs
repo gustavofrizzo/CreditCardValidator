0f8d9812887e121c40e4b2fc1f3bed98f20c43d1
gustavofrizzo <guto.smo@gmail.com>
2015-07-04 01:28:36 -0300

    Added tests.

---
+++ CreditCardUnitTest/CreditCardStringExtensionUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CreditCardUnitTest
+{
+    [TestClass]
+    public class CreditCardStringExtensionUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
