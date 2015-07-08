ec1e258de51afb2ac8a3ea619941b0cd7b3adc71
gustavofrizzo <guto.smo@gmail.com>
2015-07-08 03:56:57 -0300

    Added license.

--- CreditCardValidator/Properties/AssemblyInfo.cs
+++ CreditCardValidator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CreditCardValidator")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2015 Gustavo Frizzo")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
