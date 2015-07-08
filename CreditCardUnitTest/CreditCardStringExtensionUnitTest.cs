c6d4dca713b62cb9875bb372cd655581c150840d
gustavofrizzo <guto.smo@gmail.com>
2015-07-08 01:53:28 -0300

    Testing CreditCardStringExtension.

--- CreditCardUnitTest/CreditCardStringExtensionUnitTest.cs
+++ CreditCardUnitTest/CreditCardStringExtensionUnitTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CreditCardValidator;
 
 namespace CreditCardUnitTest
 {
@@ -7,8 +8,21 @@ namespace CreditCardUnitTest
     public class CreditCardStringExtensionUnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Validating_CreditCardBrand()
         {
+            Assert.AreEqual(CardIssuer.MasterCard, "5239088204232455".CreditCardBrand());
+        }
+
+        [TestMethod]
+        public void Validating_CreditCardBrandName()
+        {
+            Assert.AreEqual(CardIssuer.MasterCard.ToString(), "5239088204232455".CreditCardBrandName());
+        }
+
+        [TestMethod]
+        public void Validating_ValidCreditCardBrand()
+        {
+            Assert.IsTrue("5239088204232455".ValidCreditCardBrand(CardIssuer.MasterCard));
         }
     }
 }
