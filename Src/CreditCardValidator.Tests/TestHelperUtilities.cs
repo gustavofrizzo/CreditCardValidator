6fdbc8009f8c058993d870c326e518b8f01bea69
Pure Krome <github@world-domination.com.au>
2015-08-23 09:36:41 +1000

    Started refactoring tests to use xUnit.

---
+++ Src/CreditCardValidator.Tests/TestHelperUtilities.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace CreditCardUnitTest
+{
+    public static class TestHelperUtilities
+    {
+        public static TheoryData<KeyValuePair<string, string[]>> ValidCardData
+        {
+            get
+            {
+                var json = File.ReadAllText("Data\\ValidCards.json");
+                var cards = JsonConvert.DeserializeObject<Dictionary<string, string[]>>(json);
+
+                var data = new TheoryData<KeyValuePair<string, string[]>>();
+                foreach (var item in cards)
+                {
+                    data.Add(item);
+                }
+
+                return data;
+            }
+        }
+    }
+}
